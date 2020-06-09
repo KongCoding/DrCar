ade69d621469b62ba97187cf1829789f2b717aa4
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2020-06-09 16:16:27 -0400

    Add Logic (Unfinished)
    Try to add keyboard listener(unfinished)

--- src/main/java/DiscoveryGUI.java
+++ src/main/java/DiscoveryGUI.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
 
 public class DiscoveryGUI {
     private Page discoveryControl;
@@ -202,6 +199,26 @@ public class DiscoveryGUI {
 
         @Override
         public void windowDeactivated(WindowEvent e) {
+
+        }
+    }
+
+    class KeyL implements KeyListener{
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            if(e.equals(KeyEvent.VK_ENTER)){
+                currentAImode.enter();
+            }
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
 
         }
     }
