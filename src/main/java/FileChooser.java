81f39fc99f8d207c1ddd735476f42c6ac710dd87
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2020-06-19 22:23:29 -0400

    Try some online tools for Photo Identification(Just a Test!!!) 1. Use baidu cloud to identify (I didn't find complete models in google) 2. The problem is that the returned answer is in Chinese. I'm looking for some ways to make it return English. 3. There is a new main method in FileChooser.java. It's just a test. 4. A reasonable way to setting a Map/xml to translate them into English(This can be done if we only have 25 cars) 5. This service require new packages. Please download them via pom.xml.

--- src/main/java/FileChooser.java
+++ src/main/java/FileChooser.java
@@ -1,5 +1,12 @@
+import com.baidu.aip.imageclassify.AipImageClassify;
+import com.baidu.aip.util.Util;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.swing.*;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class FileChooser {
     private String path = "";
@@ -8,6 +15,22 @@ public class FileChooser {
     public FileChooser(DiscoveryService ds){
         discoveryControl = ds;
         openGUI();
+    }
+
+    public static void main(String[] args) {
+        String fileName = "Honda-CRV.jpg";
+        AipImageClassify aip = new AipImageClassify("20497789", "1W5Y9LcU5Tb5CohbMOuywWOE",
+                "ce4fVAOohFw326Kv5BT6R0UjEB283Ia6");
+        JSONObject res = null;
+        try{
+            res = aip.carDetect(Util.readFileByBytes(fileName), new HashMap<>(){{put("language", "en");}});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONArray result = res.getJSONArray("result");
+        JSONObject first = (JSONObject) result.get(0);
+        String name = (String)first.get("name");
+        System.out.println(name);
     }
 
     private void openGUI(){
