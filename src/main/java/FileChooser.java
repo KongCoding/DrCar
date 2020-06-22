5723360a213ec633eb08b3c185fd71ac87f63869
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2020-06-22 17:26:31 -0400

    Upload some pictures to train (Very Important!!!) 1. Everyone can upload a picture of one of your cars and modify the string (line 28, FileChooser.java) to that picture's path. and run that main method. There will be three messages. First is its English name. If it is nothing, that means there is not a proper English name in PictureIdentification.xml. Please add in this file. Second is Chinese name which is the first result returned by the baidu service. The last is the whole results which store multiple Chinese names. Please find top CORRECT Chinese names and add them into PictureIdentification.xml. 2. There is also a notation in PictureIdentification.xml. Please view it carefully before you start editing. 3. Everyone can upload pictures by yourselves. I strongly suggest Chinese members to help do the work that add English names into PictureIdentification.xml

--- src/main/java/FileChooser.java
+++ src/main/java/FileChooser.java
@@ -8,7 +8,6 @@ import org.json.JSONObject;
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -21,8 +20,12 @@ public class FileChooser {
         openGUI();
     }
 
+    /**
+     * Used as a train method to check each car's possible Chinese answer and write it and corresponding English into xml file.
+     * @param args
+     */
     public static void main(String[] args) {
-        String fileName = "2019-Volvo-XC90-Armoured-Heavy-01.jpg";
+        String fileName = "Pictures/Volvo/Train/2020_volvo_xc60_angularfront.jpg";
         AipImageClassify aip = new AipImageClassify("20497789", "1W5Y9LcU5Tb5CohbMOuywWOE",
                 "ce4fVAOohFw326Kv5BT6R0UjEB283Ia6");
         JSONObject res = null;
@@ -34,8 +37,14 @@ public class FileChooser {
         JSONArray result = res.getJSONArray("result");
         JSONObject first = (JSONObject) result.get(0);
         String name = (String)first.get("name");
-        System.out.println(EnglishName(name));
-        System.out.println(name);
+        /*
+        The two lines below is for programmers to check whether the English name is what they want.
+         */
+        System.out.println("English name: " + EnglishName(name));
+        System.out.println("Chinese name: "+ name);
+
+        //The line below is for programmers to view all possible Chinese names it could have.
+        System.out.println(res.toString());
     }
 
     private static String EnglishName(String ChineseName){
