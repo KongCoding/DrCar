cb05dff2dab25c3d7541e06e948a993be503aa43
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2020-07-24 12:10:01 -0400

    Add topics of Honda Fit 1. Add topics about Honda Fit to Topics.xml and all has been tested. I use the description of ACC as the answer for Driving Assistance Features while I'm not definitely sure it really is.

--- src/main/java/Tool.java
+++ src/main/java/Tool.java
@@ -6,22 +6,23 @@ import java.util.List;
 
 public class Tool {
     public static void main(String[] args) {
-        String capacity_output_torque_tp_a = "Max Output 190 hp Engine capacity 1.5 L Max torque 179 lb-ft Top speed 130 mph Acceleration, " +
-                "0-69 mph 7.6 sec";
-        String price = "Prices starting at $25,050";
-        String wheelbase_dimension = "Wheelbase is 104.7”. Dimensions are 182\" L x 73” W x 66-67\" H";
-        String ab1 = "CMBS can help bring your vehicle to a stop by automatically applying brake pressure when the " +
-                "system determines that a frontal collision is unavoidable. With your hands on the steering wheel, " +
-                "long highway drives are easier with LKAS, which subtly adjusts steering to help keep your vehicle " +
-                "centered in a detected lane.";
-        String acc = "D - Adaptive Cruise Control (ACC) 17 ACC helps you maintain a set following interval behind a " +
-                "detected vehicle for highway driving. 1 280 hp @6000 rpm ( SAE net ) . " +
-                "2 Maximum towing capacity for AWD models is 5,000 lbs. Maximum towing capacity for 2WD models is 3,500 lbs.";
+        String capacity_output_torque_tp_a = "Max Output 130 hp Engine capacity 1.5 L Max torque 114 lb-ft Top speed " +
+                "115 mph Acceleration, 0-69 mph 10 sec";
+        String price = "Prices start at $16,190";
+        String wheelbase_dimension = "Wheelbase is 99.6”. Dimensions are 161\" L x 67” W x 60\" H\n";
+        String daf = "ACC helps you maintain a set following interval behind a detected vehicle for highway driving. " +
+                "Fit for Anything The Fit can take all kinds of people and all kinds of cargo all kinds of places, " +
+                "thanks to brilliant engineering and a little magic. The spacious interior has room for five, " +
+                "and the 2nd-row Magic Seat® helps you configure your Fit four different ways.";
+        String dm = "Utility Mode Fold the Magic Seat down and you’ve got an impressive 52.7 cubic feet of maximum " +
+                "cargo space.6 It’s amazing what fits in there. Long Mode Fold down the right side of the rear seat " +
+                "and fold back the front passenger seat, and you can haul gear that’s up to 7 feet, 9 inches long. " +
+                "Tall Mode Sometimes you need a little extra room for taller items.";
         String rawString = "The 360˚ SurroundView Camera " +
                 "available in the Volvo V60 helps make parking simple by giving " +
                 "you a bird's-eye view of your car and its immediate surroundings. " +
                 "It's especially useful when fitting into tight spaces, and can help you to avoid parking lot scrapes.";
-        System.out.println(cutStringTest(acc, 4, 131));
+        System.out.println(cutStringTest(daf, 0, 244));
     }
 
     public static String cutStringTest(String answer, int start, int end){
