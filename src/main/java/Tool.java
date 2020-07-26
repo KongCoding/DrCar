2ba07e0b3711e9cad0e6cdb0209f3b72b8e07feb
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2020-07-26 16:57:33 -0400

    Add topics of Honda Ridgeline

--- src/main/java/Tool.java
+++ src/main/java/Tool.java
@@ -6,22 +6,22 @@ import java.util.List;
 
 public class Tool {
     public static void main(String[] args) {
-        String capacity_output_torque_tp_a = "Max Output 151 hp Engine capacity 1.5 L Max torque 197 lb-ft Top " +
-                "speed 125 mph Acceleration, 0-60 mph 7.3 sec";
-        String price = "Prices starting at $22,930";
+        String capacity_output_torque_tp_a = "Max output 280 hp Engine capacity 3.5 L Max torque 262 lb-ft " +
+                "Max speed 120 mph Acceleration, 0-69 mph 7.5 sec";
+        String price = "Prices start at $29,990";
         String wheelbase_dimension = "Wheelbase is 99.6”. Dimensions are 161\" L x 67” W x 60\" H\n";
         String daf = "ACC helps you maintain a set following interval behind a detected vehicle for highway driving. " +
                 "Fit for Anything The Fit can take all kinds of people and all kinds of cargo all kinds of places, " +
                 "thanks to brilliant engineering and a little magic. The spacious interior has room for five, " +
                 "and the 2nd-row Magic Seat® helps you configure your Fit four different ways.";
-        String acc = "B - Adaptive Cruise Control (ACC) with Low-Speed Follow 5 ACC helps you maintain a set " +
-                "following interval behind detected vehicles for highway driving. If the detected vehicle slows to " +
-                "a stop, ACC is designed to slow and stop your vehicle as well.";
+        String acc = "Each element pushes the limit of what a truck can do to advance its function and expand your " +
+                "possibilities. In-Bed Trunk • Only pickup with a lockable trunk • 7.3 cubic feet of secure storage " +
+                "5 • Use as a cooler or to stow your dirty gear • Drain plug for quick water removal and easy cleaning";
         String rawString = "The 360˚ SurroundView Camera " +
                 "available in the Volvo V60 helps make parking simple by giving " +
                 "you a bird's-eye view of your car and its immediate surroundings. " +
                 "It's especially useful when fitting into tight spaces, and can help you to avoid parking lot scrapes.";
-        System.out.println(cutStringTest(acc, 58, 0));
+        System.out.println(cutStringTest(acc, 108, 0));
     }
 
     public static String cutStringTest(String answer, int start, int end){
@@ -29,8 +29,7 @@ public class Tool {
         if(start >= end || end > answer.length())
             return "Error, please check your start and end, total length = " + answer.length();
         else{
-            String returnS = "Correct, clean answer: " + answer.substring(start, end) + "\nTotal length = " + answer.length();
-            return returnS;
+            return "Correct, clean answer: " + answer.substring(start, end) + "\nTotal length = " + answer.length();
         }
     }
 
@@ -42,6 +41,7 @@ public class Tool {
             return answer.substring(start, end);
     }
 
+    @SuppressWarnings("all")
     public static String[] returnAllCars() throws Exception{
         ArrayList<String> cars = new ArrayList<>();
         Document topics = DiscoveryService.load("Topics.xml");
@@ -53,6 +53,7 @@ public class Tool {
         return cars.toArray(new String[cars.size()]);
     }
 
+    @SuppressWarnings("all")
     public static Element returnGivenCarTopics(String carName) throws Exception{
         ArrayList<String> topic = new ArrayList<>();
         Document topics = DiscoveryService.load("Topics.xml");
@@ -65,11 +66,11 @@ public class Tool {
 //        return topic.toArray(new String[topic.size()]);
         return allTopic;
     }
-
+    @SuppressWarnings("all")
     public static String returnHeadLetter(Element topic) throws Exception{
         return topic.element("head-letters").getText();
     }
-
+    @SuppressWarnings("all")
     public static int[] returnStartAndEnd(Element topic) throws Exception{
         return new int[]{Integer.parseInt(topic.element("start").getText()), Integer.parseInt(topic.element("end").getText())};
     }
