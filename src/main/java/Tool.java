91d6f9e1d2be170b8769ff17e85ee271c44993f7
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2020-07-25 20:29:14 -0400

    Add topics of Honda Insight

--- src/main/java/Tool.java
+++ src/main/java/Tool.java
@@ -6,23 +6,22 @@ import java.util.List;
 
 public class Tool {
     public static void main(String[] args) {
-        String capacity_output_torque_tp_a = "Max Output 130 hp Engine capacity 1.5 L Max torque 114 lb-ft Top speed " +
-                "115 mph Acceleration, 0-69 mph 10 sec";
-        String price = "Prices start at $16,190";
+        String capacity_output_torque_tp_a = "Max Output 151 hp Engine capacity 1.5 L Max torque 197 lb-ft Top " +
+                "speed 125 mph Acceleration, 0-60 mph 7.3 sec";
+        String price = "Prices starting at $22,930";
         String wheelbase_dimension = "Wheelbase is 99.6”. Dimensions are 161\" L x 67” W x 60\" H\n";
         String daf = "ACC helps you maintain a set following interval behind a detected vehicle for highway driving. " +
                 "Fit for Anything The Fit can take all kinds of people and all kinds of cargo all kinds of places, " +
                 "thanks to brilliant engineering and a little magic. The spacious interior has room for five, " +
                 "and the 2nd-row Magic Seat® helps you configure your Fit four different ways.";
-        String dm = "Utility Mode Fold the Magic Seat down and you’ve got an impressive 52.7 cubic feet of maximum " +
-                "cargo space.6 It’s amazing what fits in there. Long Mode Fold down the right side of the rear seat " +
-                "and fold back the front passenger seat, and you can haul gear that’s up to 7 feet, 9 inches long. " +
-                "Tall Mode Sometimes you need a little extra room for taller items.";
+        String acc = "B - Adaptive Cruise Control (ACC) with Low-Speed Follow 5 ACC helps you maintain a set " +
+                "following interval behind detected vehicles for highway driving. If the detected vehicle slows to " +
+                "a stop, ACC is designed to slow and stop your vehicle as well.";
         String rawString = "The 360˚ SurroundView Camera " +
                 "available in the Volvo V60 helps make parking simple by giving " +
                 "you a bird's-eye view of your car and its immediate surroundings. " +
                 "It's especially useful when fitting into tight spaces, and can help you to avoid parking lot scrapes.";
-        System.out.println(cutStringTest(daf, 0, 244));
+        System.out.println(cutStringTest(acc, 58, 0));
     }
 
     public static String cutStringTest(String answer, int start, int end){
